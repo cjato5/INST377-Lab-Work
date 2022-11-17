6cde61e50312cc0b4e8e7fcf1495d668e466278a
cjato5 <cjato@terpmail.umd.edu>
2022-11-17 16:19:51 -0500

    lab3

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -57,6 +57,7 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+
   updateSlidePosition();
 }
 
